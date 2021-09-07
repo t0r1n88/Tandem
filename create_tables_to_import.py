9dedcfca6a3a3ec53fb3fc024a1d297c4813a7b5
Oleg Budaev <t0r1n88@mail.ru>
2021-09-07 14:20:33 +0800

    Begin

---
+++ create_tables_to_import.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Создаем таблицы для импорта в тандем
+# Считываем в датафреймы базовые документы
+
+person_df = pd.read_excel('resources/person_t_columns.xlsx')
+group_df = pd.read_excel('resources/group_t_columns.xlsx')
+student_df = pd.read_excel('resources/student_t_columns.xlsx')
+
+
+
+
